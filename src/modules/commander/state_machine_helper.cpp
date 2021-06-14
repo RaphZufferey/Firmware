39d4f6c5c94dc4a1790c48ebb3b6312f7b2ac94e
RaphZufferey <30928170+RaphZufferey@users.noreply.github.com>
2021-06-14 19:04:17 +0200

    state helper added sail stuff

--- src/modules/commander/state_machine_helper.cpp
+++ src/modules/commander/state_machine_helper.cpp
@@ -296,6 +296,7 @@ main_state_transition(const vehicle_status_s &status, const main_state_t new_mai
 	case commander_state_s::MAIN_STATE_MANUAL:
 	case commander_state_s::MAIN_STATE_STAB:
 	case commander_state_s::MAIN_STATE_ACRO:
+	case commander_state_s::MAIN_STATE_SAIL:
 		ret = TRANSITION_CHANGED;
 		break;
 
@@ -459,6 +460,7 @@ bool set_nav_state(vehicle_status_s &status, actuator_armed_s &armed, commander_
 	case commander_state_s::MAIN_STATE_MANUAL:
 	case commander_state_s::MAIN_STATE_STAB:
 	case commander_state_s::MAIN_STATE_ALTCTL:
+	case commander_state_s::MAIN_STATE_SAIL:
 
 		/* require RC for all manual modes */
 		if (rc_lost && is_armed) {
@@ -482,6 +484,10 @@ bool set_nav_state(vehicle_status_s &status, actuator_armed_s &armed, commander_
 
 			case commander_state_s::MAIN_STATE_ALTCTL:
 				status.nav_state = vehicle_status_s::NAVIGATION_STATE_ALTCTL;
+				break;
+
+			case commander_state_s::MAIN_STATE_SAIL:
+				status.nav_state = vehicle_status_s::NAVIGATION_STATE_SAIL;
 				break;
 
 			default:
